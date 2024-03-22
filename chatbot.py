b76e02502bfd5e36b2a77a01ec39e41d1c9b23b2
unknown <manuacostaok@gmail.com>
2024-03-22 16:44:18 -0300

    minor fix

--- chatbot.py
+++ chatbot.py
@@ -1,21 +1,21 @@
 from nltk.chat.util import Chat, reflections
-mis_reflexions = {
+reflecciones = {
 "ir": "fui",
 "hola": "hey"
 }
 
-pares = [
+respuestas = [
     [
         r"se me ha caido el hosting (.*)",
         ["Sentimos ese fallo, para reiniciarlo, entra en CPANEL y selecciona reiniciar",]
     ],
      [
         r"cuando hay que pagar la factura (.*)",
-        ["Hay que pagarla el dia 15 de cada mes por tarjeta de crédito",]
+        ["Hay que pagarla el dia 15 de cada mes por cualquier metodo de pago, o una vez por año si elegiste el servicio anual",]
     ],
     [
         r"(.*) ampliar el servicio",
-        ["Para ampliar el servicio, contacta con facturacion",]
+        ["Para ampliar el servicio, contacta con atención al cliente",]
     ],
     [
         r"disculpa (.*)",
@@ -27,30 +27,30 @@ pares = [
     ],
     [
         r"que (.*) quieres ?",
-        ["Nada gracias",]
+        ["Nada, gracias",]
         
     ],
     [
         r"(.*) creado ?",
-        ["Fui creado hoy",]
+        ["Fui creado luego del bigbang pero mi codigo fuente fue descubierto en 2024",]
     ],
     [
         r"finalizar",
-        ["Chao","Fue bueno hablar contigo"]
+        ["Chao","espero haberte ayudado"]
 ],
 ]
 def chatear():
     bandera=True
-    print("Hola, soy el servicio de hosting. Si quieres salir tipee Adios") #mensaje por defecto
+    print("Hola, soy el bot de servicio de hosting, mi nombre es Cosme Fulanito. En que puedo ayudarte? Si quieres salir tipee Exit") #mensaje por defecto
     while(bandera==True):
         respuesta_usuario = input()
         respuesta_usuario=respuesta_usuario.lower()
-        if(respuesta_usuario!='adios'):        
-            chat = Chat(pares, mis_reflexions)
+        if(respuesta_usuario!='exit'):        
+            chat = Chat(respuestas, reflecciones)
             chat.converse()
         else:
             bandera=False
-            print("Adios, que ande bien.") 
+            print("Hasta luego, que ande bien.") 
             exit()
 
 if __name__ == "__main__":
