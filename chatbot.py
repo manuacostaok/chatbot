301eb26926573013f4c656db7cf51789213251db
Manu Acosta <109755907+manuacostaok@users.noreply.github.com>
2024-03-24 13:52:53 -0300

    update

--- chatbot.py
+++ chatbot.py
@@ -1,17 +1,21 @@
 from nltk.chat.util import Chat, reflections
-reflecciones = {
+mis_reflecciones = {
 "ir": "fui",
 "hola": "hey"
 }
 
-respuestas = [
+pares = [
     [
-        r"se cayó el servicio de hosting (.*)",
-        ["Sentimos ese fallo, para reiniciarlo, entra en 'Panel de control' y selecciona 'Reiniciar'",]
+        r"se cayó el servicio de internet (.*)|mi internet no anda(.*)|no me anda internet(.*)|no tengo internet|(.*)",
+        ["Sentimos ese fallo, puede reiniciar su modem desenchufandolo por unos segundos y dejando que las luces enciendan, estaremos checkeando que nuestro sistema no tenga problemas, si ya reinicio el modem aguarde"]
     ],
-     [
+    [
         r"cuando hay que pagar la factura (.*)",
-        ["Hay que pagarla el dia 15 de cada mes por cualquier metodo de pago, o una vez por año si elegiste el servicio anual",]
+        ["Hay que pagarla el dia 15 de cada mes por cualquier método de pago, o una vez por año si elegiste el servicio anual, también puede adherirla al debito automático",]
+    ],
+    [
+        r"ya reinicie mi modem y sigo sin internet (.*)",
+        ["A continuacion, digite su numero de dni para poder chequear que todo esté en orden, de no ser así nos estaremos contactado por su telefono celular para brindarle mejor atención ",]
     ],
     [
         r"(.*) ampliar el servicio",
@@ -41,15 +45,15 @@ respuestas = [
 ]
 def chatear():
     bandera=True
-    print("Hola, soy el bot de servicio de hosting, mi nombre es Cosme Fulanito. En que puedo ayudarte? Si quieres salir tipee Exit") #mensaje por defecto
+    print("Hola, soy el bot de servicio de hosting, mi nombre es Cosme Fulanito. En que puedo ayudarte? Si quieres salir escriba Exit") #mensaje por defecto
     while(bandera==True):
         respuesta_usuario = input()
         respuesta_usuario=respuesta_usuario.lower()
         if(respuesta_usuario!='exit'):        
-            chat = Chat(respuestas, reflecciones)
+            chat = Chat(pares, mis_reflecciones)
             chat.converse()
         else:
-            print("Hasta luego, que ande bien.") 
+            print("Hasta luego, espero haberte ayudado, saludos.") 
             bandera=False
             exit()
 
