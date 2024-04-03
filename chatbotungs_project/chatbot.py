c85d86780098d4228dbeb298d8c73aff69dee596
Alfonso J Lombardi <alfonsojavierlombardi@gmail.com>
2024-04-03 14:25:43 -0300

    fixes in sklearn - changes in readme

--- chatbotungs_project/chatbot.py
+++ chatbotungs_project/chatbot.py
@@ -11,104 +11,51 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.svm import SVC #machinelearn
 from sklearn.decomposition import PCA #machinelearn
-# Importar las reflecciones y el chat de NLTK
-from nltk.chat.util import reflections
 
 import nltk
 nltk.download('wordnet')
 nltk.download('punkt')
 
-# Reflecciones para NLTK
-mis_reflecciones = {
-    "ir": "fui",
-    "hola": "hey",
-}
 
 # Definir el lematizador de NLTK
 lemmatizer = WordNetLemmatizer()
 
-#setup array con palabras clave
-arrayOneWord = ['hola','hey','buenas','nada','salir','chau','adios','exit','perdon','perdona','ok','oka']
 # Pares de patrones y respuestas
-pares = [
-    [
-        r"(.*)se cayó el servicio(.*)|(.*)mi internet no anda(.*)|(.*)no me anda internet(.*)|(.*)no tengo internet(.*)",
-        ["Sentimos ese fallo, puede reiniciar su modem desenchufándolo por unos segundos y dejando que las luces enciendan, estaremos chequeando que nuestro sistema no tenga problemas, si ya reinicio el modem aguarde"]
-    ],
-    [
-       r"(.*)Como puedo proteger mi red Wi-Fi(.*)|(.*)conectarse sin autorización?" ,
-       ["Puedes proteger tu red Wi-Fi utilizando una contraseña segura y habilitando la encriptación WPA2 o WPA3. También puedes desactivar la difusión del nombre de tu red (SSID) para que no sea visible para otros dispositivos cercanos."]
-    ],
-    [
-        r"(.*)Mi conexión a internet es más lenta en ciertas habitaciones de mi casa(.*)",
-        ["Esto podría deberse a una señal Wi-Fi débil en esas áreas. Intenta mover el enrutador a un lugar más central en tu casa o utiliza un extensor de alcance Wi-Fi para mejorar la cobertura en esas áreas."]
-    ],
-    [
-        r"¿Por qué mi velocidad de internet es más lenta de lo que debería ser según mi plan?|(.*)internet lento(.*)",
-        ["La velocidad de internet puede verse afectada por varios factores, como la congestión de la red, problemas con el enrutador o cables dañados. Te recomendaría realizar una prueba de velocidad en diferentes momentos del día y, si la velocidad es consistentemente baja, contactar a nuestro servicio de atención al cliente para que podamos investigar más a fondo."]
-    ],
-    [
-        r"(.*)bloquear ciertos sitios web en mi red(.*)",
-        ["Puedes bloquear ciertos sitios web en tu red Wi-Fi utilizando la función de control parental en tu enrutador o utilizando software de filtrado de contenido. Consulta la documentación de tu enrutador para obtener instrucciones específicas."]
-    ],
-    [
-        r"(.*)cuando hay que pagar la factura(.*)",
-        ["Hay que pagarla el día 15 de cada mes por cualquier método de pago, o una vez por año si elegiste el servicio anual, también puede adherirla al débito automático",]
-    ],
-    [
-        r"(.*)ya reinicie mi modem y sigo sin internet(.*)",
-        ["A continuación, digite su número de DNI para poder chequear que todo esté en orden, de no ser así nos estaremos contactando por su teléfono celular para brindarle mejor atención ",]
-    ],
-    [
-        r"(.*)ampliar el servicio",
-        ["Para ampliar el servicio contacta con atención al cliente",]
-    ],
-    [
-        r"disculpa(.*)|(.*)perdon",
-        ["Estoy aquí para ayudarte, no para perdonarte",]
-    ],
-    [
-        r"hola|hey|buenas|que onda",
-        ["Hola, ¿en qué puedo ayudarte?",]
-    ],
-    [
-        r"(.*)que quieres?|(.*)como estas?",
-        ["Nada, estoy bien, solo quiero ayudarte, gracias",]
-    ],
-    [
-        r"nada",
-        ["No dijiste nada, ¿podrías volver a intentarlo?",]
-    ],
-    [
-        r"(.*)cuando fuiste creado?",
-        ["Fui creado luego del Big Bang pero mi código fuente fue descubierto en 2024",]
-    ],
-    [
-        r"salir|chau|adios",
-        ["Chau, espero haberte ayudado, recuerda que para salir del chat debes escribir 'EXIT'"]
-    ],
-]
-
-# Función para preprocesar el texto del usuario
-def preprocess_text(sentence):
-    # Tokenización
-    tokens = word_tokenize(sentence)
-    # Lematización
-    lemmatized_tokens = [lemmatizer.lemmatize(token.lower()) for token in tokens]
-    return " ".join(lemmatized_tokens)
-
-# Crear un vectorizador TF-IDF para clasificación de intenciones
-vectorizer = TfidfVectorizer(preprocessor=preprocess_text)
-preguntas = [pair[0] for pair in pares]
-preguntas_vect = vectorizer.fit_transform(preguntas)
-
-# Función para clasificar la intención del usuario usando scikit-learn
-def clasificar_intencion(respuesta_usuario):
-    respuesta_usuario = preprocess_text(respuesta_usuario)
-    respuesta_usuario_vect = vectorizer.transform([respuesta_usuario])
-    similitud = cosine_similarity(respuesta_usuario_vect, preguntas_vect)
-    idx = similitud.argmax()
-    return pares[idx][1][0]
+# Diccionario de preguntas y respuestas
+qa_pairs = {
+    "Como puedo proteger mi red Wi-Fi": "Puedes proteger tu red Wi-Fi utilizando una contraseña segura y habilitando la encriptación WPA2 o WPA3. También puedes desactivar la difusión del nombre de tu red (SSID) para que no sea visible para otros dispositivos cercanos.",
+    "conectarse sin autorización": "Puedes proteger tu red Wi-Fi utilizando una contraseña segura y habilitando la encriptación WPA2 o WPA3. También puedes desactivar la difusión del nombre de tu red (SSID) para que no sea visible para otros dispositivos cercanos.",
+    "Mi conexión wifi es más lenta en ciertas habitaciones de mi casa": "Esto podría deberse a una señal Wi-Fi débil en esas áreas. Intenta mover el enrutador a un lugar más central en tu casa o utiliza un extensor de alcance Wi-Fi para mejorar la cobertura en esas áreas.",
+    "el wifi esta lenta": "Esto podría deberse a una señal Wi-Fi débil en esas áreas. Intenta mover el enrutador a un lugar más central en tu casa o utiliza un extensor de alcance Wi-Fi para mejorar la cobertura en esas áreas.Si luego no mejora el dispositivo puede tener la antena dañada",
+    "mi wifi tiene poco potencia": "Esto podría deberse a una señal Wi-Fi débil en esas áreas. Intenta mover el enrutador a un lugar más central en tu casa o utiliza un extensor de alcance Wi-Fi para mejorar la cobertura en esas áreas. Si luego no mejora el dispositivo puede tener la antena dañada",
+    "¿Por qué mi velocidad de internet es más lenta de lo que debería ser según mi plan?": "La velocidad de internet puede verse afectada por varios factores, como la congestión de la red, problemas con el enrutador o cables dañados. Te recomendaría realizar una prueba de velocidad en diferentes momentos del día y, si la velocidad es consistentemente baja, contactar a nuestro servicio de atención al cliente para que podamos investigar más a fondo.",
+    "internet lento": "La velocidad de internet puede verse afectada por varios factores, como la congestión de la red, problemas con el enrutador o cables dañados. Te recomendaría realizar una prueba de velocidad en diferentes momentos del día y, si la velocidad es consistentemente baja, contactar a nuestro servicio de atención al cliente para que podamos investigar más a fondo.",
+    "la conexión es lenta": "La velocidad de internet puede verse afectada por varios factores, como la congestión de la red, problemas con el enrutador o cables dañados. Te recomendaría realizar una prueba de velocidad en diferentes momentos del día y, si la velocidad es consistentemente baja, contactar a nuestro servicio de atención al cliente para que podamos investigar más a fondo.",
+    "bloquear ciertos sitios web en mi red": "Puedes bloquear ciertos sitios web en tu red Wi-Fi utilizando la función de control parental en tu enrutador o utilizando software de filtrado de contenido. Consulta la documentación de tu enrutador para obtener instrucciones específicas.",
+    "se cayó el servicio": "Sentimos ese fallo, puede reiniciar su modem desenchufándolo por unos segundos y dejando que las luces enciendan, estaremos chequeando que nuestro sistema no tenga problemas, si ya reinicio el modem aguarde",
+    "no funciona el servicio": "Sentimos ese fallo, puede reiniciar su modem desenchufándolo por unos segundos y dejando que las luces enciendan, estaremos chequeando que nuestro sistema no tenga problemas, si ya reinicio el modem aguarde",
+    "ya reinicie mi modem y sigo sin internet": "A continuación, digite su número de DNI para poder chequear que todo esté en orden, de no ser así nos estaremos contactando por su teléfono celular para brindarle mejor atención ",
+    "como colocar Wi-Fi en mi hogar": "Para colocar Wi-Fi en su hogar contacta con atención al cliente que le indicara un plan adecuado a sus necesidades 0800-555-2323 ",
+    "mi plan": "Para conocer las caracteristicas de su plan contacta con atención al cliente 0800-555-2323",
+    "mi servicio": "Para conocer el servicio que posee contacta con atención al cliente 0800-555-2323",
+    "ampliar el servicio": "Para ampliar el servicio contacta con atención al cliente 0800-555-2323",
+    "mejorar plan": "Para ampliar el servicio contacta con atención al cliente 0800-555-2323",
+    "pagar la factura":"Hay que pagarla el día 15 de cada mes por cualquier método de pago, o una vez por año si elegiste el servicio anual, también puede adherirla al débito automático",
+    "disculpa": "Estoy aquí para ayudarte, no para perdonarte",
+    "perdon": "Estoy aquí para ayudarte, no para perdonarte",
+    "hola": "Hola, ¿en qué puedo ayudarte?",
+    "hey": "Hola, ¿en qué puedo ayudarte?",
+    "buenas": "Hola, ¿en qué puedo ayudarte?",
+    "que onda": "Hola, ¿en qué puedo ayudarte?",
+    "que quieres?": "Nada, estoy bien, solo quiero ayudarte, gracias",
+    "como estas?": "Nada, estoy bien, solo quiero ayudarte, gracias",
+    "nada": "No dijiste nada, ¿podrías volver a intentarlo?",
+    "cuando fuiste creado?": "Fui creado luego del Big Bang pero mi código fuente fue descubierto en 2024",
+    "salir": "Chau, espero haberte ayudado, recuerda que para salir del chat debes escribir 'EXIT'",
+    "chau": "Chau, espero haberte ayudado, recuerda que para salir del chat debes escribir 'EXIT'",
+    "adios": "Chau, espero haberte ayudado, recuerda que para salir del chat debes escribir 'EXIT'",
+    "exit": "Chau, espero haberte ayudado, recuerda que para salir del chat debes escribir 'EXIT'"
+}
 
 def process_fingerprint_images(image_path):
     # Ruta de la imagen local
@@ -170,20 +117,44 @@ def process_fingerprint_images(image_path):
 #En este ejemplo, primero usamos PCA para reducir la dimensionalidad de las imágenes de huellas digitales y la imagen local. 
 #    Luego, entrenamos un clasificador SVM con las características extraídas de la imagen local.
 #     Finalmente, utilizamos el clasificador para predecir si la huella digital es similar o diferente a la imagen local.
-   
+
+def preprocess_text(sentence):
+    # Tokenización y lematización
+    tokens = word_tokenize(sentence)
+    lemmatized_tokens = [lemmatizer.lemmatize(token.lower()) for token in tokens ]
+    return " ".join(lemmatized_tokens)
+
+# Crear un vectorizador TF-IDF para clasificación de intenciones
+vectorizer = TfidfVectorizer(preprocessor=preprocess_text)
+preguntas = list(qa_pairs.keys())
+print("preguntas")
+print(preguntas)
+preguntas_vect = vectorizer.fit_transform(preguntas)
+print('preguntas_vect')
+print(preguntas_vect)
+
+# Etiquetas de las intenciones
+etiquetas = [idx for idx in range(len(qa_pairs))]
+
+# Entrenar un clasificador SVM para clasificar la intención del usuario
+clasificador = SVC(kernel='linear')
+clasificador.fit(preguntas_vect, etiquetas)
+
+# Función para clasificar la intención del usuario
+def clasificar_intencion(respuesta_usuario):
+    respuesta_usuario = preprocess_text(respuesta_usuario)
+    respuesta_usuario_vect = vectorizer.transform([respuesta_usuario])
+    return clasificador.predict(respuesta_usuario_vect)[0]
+
+# Función para generar respuestas
+def generar_respuesta(intencion):
+    return list(qa_pairs.values())[intencion]
+
 # Función para chatear con el usuario
-def chatear(respuesta_usuario):
-    if respuesta_usuario.lower() == 'exit':
-        respuesta_bot = "exit"
-    elif respuesta_usuario.strip():  # Verificar si la entrada tiene sentido
-        if len(respuesta_usuario.split()) == 1 and respuesta_usuario not in arrayOneWord :  # Verificar si la entrada tiene solo una palabra
-            respuesta_bot = "Lo siento, no logro comprender la pregunta. Por favor, intenta proporcionar más detalles."
-        elif "login huella" in respuesta_usuario.lower():
-            # Llamar a la función process_fingerprint_images con la ruta del archivo de huella digital
-            respuesta_bot = process_fingerprint_images('./staticfiles/img/huellas_dataset/101_2.tif')
-            print("Bot:", respuesta_bot)  # Imprimir la respuesta del bot
-        else:
-            respuesta_bot = clasificar_intencion(respuesta_usuario)
+def chatear(respuesta_usuario):     
+    if respuesta_usuario.strip():  # Verificar si la entrada tiene sentido 
+        intencion = clasificar_intencion(respuesta_usuario)
+        respuesta_bot = generar_respuesta(intencion)
     else: # Verificar si la entrada no está vacía
         respuesta_bot = "No dijiste nada, ¿podrías volver a intentarlo?"
 
